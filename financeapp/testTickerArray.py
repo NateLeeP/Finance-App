36564e7ad929965186687ec8b8e7a651e438377f
nlpruitt <nlpruitt@gmail.com>
2020-09-28 11:38:02 -0400

    Commented out print statement

--- financeapp/testTickerArray.py
+++ financeapp/testTickerArray.py
@@ -1,5 +1,6 @@
 from financeapp import betaFunctions as bf
 from financeapp import dateLookUp as dl
+import datetime
 
 testArray = [{'Ticker':'TSLA', 'Shares':20}, {'Ticker':'F', 'Shares':50}, {'Ticker':'FB', 'Shares':30}, {'Ticker':'SPY', 'Shares':50}, {'Ticker':'AMZN', 'Shares':10}]
 
@@ -13,7 +14,7 @@ bf.getPortfolioPercentage(testArray, bf.getPortfolioValue(testArray))
 bf.getCAPM(testArray, expectedMarketReturn)
 bf.getPortfolioBeta(testArray)
 
-print(dl.price_array[:5])
+#(dl.price_array[:5])
 
 totalExpectedReturn = float("{:.2f}".format(bf.portfolioExpectedReturn(testArray)))
 newTotalPortfolioValue = bf.getPortfolioValue(testArray) * (1 + (totalExpectedReturn/100))
@@ -21,5 +22,5 @@ beginTotalPortfolioValue = bf.getPortfolioValue(testArray)
 requiredReturn = bf.calculateRequiredReturn(beginTotalPortfolioValue, newTotalPortfolioValue)
 daysToRecover = dl.daysLookUp(requiredReturn, bf.getPortfolioBeta(testArray))
 
-print(bf.getPortfolioBeta(testArray))
-print(dl.price_array[:5])
+#print(bf.getPortfolioBeta(testArray))
+#print(dl.price_array[:5])
