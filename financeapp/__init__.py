976322405da813a6ff48a10b90d475ef3ffce983
nlpruitt <nlpruitt@gmail.com>
2020-09-30 12:57:37 -0400

    Added initalization for database, login

--- financeapp/__init__.py
+++ financeapp/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
 
-from financeapp import routes
+
+from financeapp import routes, models
