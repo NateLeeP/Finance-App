d70dbc109b22fac2b65ad2b7e2cc98e07629315a
nlpruitt <nlpruitt@gmail.com>
2020-10-01 08:17:21 -0400

    Added logger object to application

--- financeapp/__init__.py
+++ financeapp/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import os
+import logging
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
@@ -11,6 +12,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.getLogger(__name__).setLevel(logging.INFO)
 
 
 from financeapp import routes, models
